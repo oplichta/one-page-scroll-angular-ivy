e47be94a630d8b5fcc18cec7a2a92f9ffa1bbe93
Oskar <oskar@rescompany.com>
2020-05-29 10:35:16 +0200

    Added new components, cleared app component template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ { path: '', component: MainComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { OneComponent } from './main/one/one.component';
+import { TwoComponent } from './main/two/two.component';
+import { ThreeComponent } from './main/three/three.component';
+import { FourComponent } from './main/four/four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    OneComponent,
+    TwoComponent,
+    ThreeComponent,
+    FourComponent
   ],
   imports: [
     BrowserModule,
